2dae17b51539393bcaa9d8e1af46e2e8be04924d
oopsys6 <operasystem6@naver.com>
2021-08-13 12:10:12 +0900

    feat: user 도메인 생성

---
+++ src/main/java/com/project/githubactionstestjagoga/domain/User.java
@@ -0,0 +1,20 @@
+package com.project.githubactionstestjagoga.domain;
+
+public class User {
+
+    private Long userId;
+    private String userName;
+
+    public User(Long userId, String userName) {
+        this.userId = userId;
+        this.userName = userName;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
