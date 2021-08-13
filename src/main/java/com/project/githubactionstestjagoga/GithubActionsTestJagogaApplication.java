bb0946b813ced12854cbe5f29d20b0553421fa80
oopsys6 <operasystem6@naver.com>
2021-08-13 12:03:11 +0900

    chore: 프로젝트 초기 셋팅

---
+++ src/main/java/com/project/githubactionstestjagoga/GithubActionsTestJagogaApplication.java
@@ -0,0 +1,13 @@
+package com.project.githubactionstestjagoga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsTestJagogaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsTestJagogaApplication.class, args);
+	}
+
+}
