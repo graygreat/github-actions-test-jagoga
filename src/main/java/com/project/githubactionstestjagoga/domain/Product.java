6f1db1dcdc6bfb0087d8eaf7f91841d5bc70ea0b
oopsys6 <operasystem6@naver.com>
2021-08-19 09:34:24 +0900

    test: test

---
+++ src/main/java/com/project/githubactionstestjagoga/domain/Product.java
@@ -0,0 +1,12 @@
+package com.project.githubactionstestjagoga.domain;
+
+public class Product {
+
+    private Long id;
+    private String name;
+
+    public Product(Long id,           String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
